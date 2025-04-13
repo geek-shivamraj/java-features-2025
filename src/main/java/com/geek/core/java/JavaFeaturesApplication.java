8b8ab95fcb7a28323b40df503c0cf51376de8fcd
Shivam-Omen <srvcode@gmail.com>
2025-04-13 10:28:07 +0530

    Adding Basic template

---
+++ src/main/java/com/geek/core/java/JavaFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.geek.core.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFeaturesApplication.class, args);
+	}
+
+}
